0b93c5a0083f9e450cdce8d3d3481be76c2ee46e
HemanthKumar-2006 <hk06resilience@gmail.com>
2026-02-17 10:13:01 +0530

    UC1: Application Entry & Welcome Message

---
+++ PalindromeChecker1/src/PalindromeChecker.java
@@ -0,0 +1,24 @@
+public class PalindromeChecker {
+    public static  void main(String[]args){
+        System.out.println("Welcome to palindrome checker management system");
+        System.out.println("Version:1.0");
+        System.out.println("System initialized sucessfully.");
+    }
+}
+
+/**
+ * ============================================================
+ * MAIN CLASS - UseCase1PalindromeCheckerApp
+ * ============================================================
+ *
+ * Use Case 1: Application Entry & Welcome Message
+
+ * The goal is to establish a clear startup flow.
+ *
+ * @author Hemanth
+ * @version 1.0
+ */
+
+
+
+
