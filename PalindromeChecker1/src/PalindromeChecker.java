9ae362ca794af2e43782a11236420102c2416973
HemanthKumar-2006 <hk06resilience@gmail.com>
2026-02-17 11:21:22 +0530

    UC2: Print a Hardcoded Palindrome Result

--- PalindromeChecker1/src/PalindromeChecker.java
+++ PalindromeChecker1/src/PalindromeChecker.java
@@ -18,6 +18,27 @@ public class PalindromeChecker {
  * @author Hemanth
  * @version 1.0
  */
+public class UseCase2PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Hardcoded string
+        String word = "madam";
+
+        // Reverse the string
+        String reversedWord = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversedWord += word.charAt(i);
+        }
+
+        // Check if palindrome
+        if (word.equals(reversedWord)) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is not a Palindrome.");
+        }
+    }
+}
 
 
 
